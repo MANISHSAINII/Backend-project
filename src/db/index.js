052af9837655f9520ccc0c6fd0a2107ffe72dea1
MANISHSAINI <MANISHSAINICOM14@gmail.com>
2025-10-10 11:45:44 +0530

    utils descration

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,6 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        
     const connectioninstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
     console.log(`/n Mongoose Connected !! DB HOST : ${connectioninstance.connection.host}`)
         } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path : "./env"
@@ -9,8 +10,7 @@ dotenv.config({
 connectDB()
 .then( () => {
     app.listen( process.env.PORT || 8000 , () => {
-        console.log(`server is running at port :
-            ${process.env.PORT }`)
+        console.log(`server is running at port : ${process.env.PORT }`)
     })
 })
 .catch( (err) => {
